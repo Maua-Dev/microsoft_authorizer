7a082346bccd72e1c188aa219d22901df740f8ce
FelipeCarillo <felipecarillo@outlook.com>
2024-06-04 11:57:11 -0300

    chore: Remove duplicate code for lambda asset in IacStack

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -18,7 +18,6 @@ class IacStack(Stack):
             runtime=_lambda.Runtime.PYTHON_3_11,
             code=_lambda.Code.from_asset("../src"),
             handler="microsoft_authorizer.lambda_handler",
-            code=_lambda.Code.from_asset("lambda/authorizer"),
             environment={
                 "GRAPH_MICROSOFT_ENDPOINT": GRAPH_MICROSOFT_ENDPOINT,
             },
